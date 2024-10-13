1e07e6ea72ddbb26e0821db1c2b99880eba84175
VeronicaObodozie <31451962+VeronicaObodozie@users.noreply.github.com>
2024-10-13 19:49:30 -0400

    Base for code
    Updating garbage classification transfer learning

--- Assignment 2/ModelGarbageClassification.py
+++ Assignment 2/ModelGarbageClassification.py
@@ -132,6 +132,43 @@ plt.show()
 ##----------Transfer Learning----------------##
 # ResNet-50 being used as a fixed feature extractor and the last fully connected layer will be trained
 
+model_conv = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 2)
+
+model_conv = model_conv.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opposed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+
+## Train and Eval
+model_conv = train_model(model_conv, criterion, optimizer_conv,
+                         exp_lr_scheduler, num_epochs=25)
+
+
+
+visualize_model(model_conv)
+
+plt.ioff()
+plt.show()
+
+
+
+
+
+
+
 #----------- Defining Model. -------------#
 ##  Transfer Learning of ResNet-50
 net = models.resnet18(pretrained=True)# Calling a pre-trained ResNet-50 model
